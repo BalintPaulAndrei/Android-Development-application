5cd7be6e335af12caa777e96b646acaafabf9339
Paul <paulbalint2000@yahoo.com>
2021-03-20 19:05:25 +0200

    Adding second activity

--- app/src/main/java/com/example/tema_dac_1/MainActivity.java
+++ app/src/main/java/com/example/tema_dac_1/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.tema_dac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
